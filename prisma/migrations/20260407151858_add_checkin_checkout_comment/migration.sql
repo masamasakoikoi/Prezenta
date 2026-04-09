bdec2a6724a5d12f44e542e662915177be40ebcd
小泉将拓 <koichan1996@icloud.com>
2026-04-09 20:30:55 +0900

    -

---
+++ prisma/migrations/20260407151858_add_checkin_checkout_comment/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Attendance" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "date" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "startTime" TEXT,
+    "finishTime" TEXT,
+    "checkIn" TEXT,
+    "checkOut" TEXT,
+    "comment" TEXT NOT NULL DEFAULT '',
+    CONSTRAINT "Attendance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Attendance" ("date", "finishTime", "id", "startTime", "status", "userId") SELECT "date", "finishTime", "id", "startTime", "status", "userId" FROM "Attendance";
+DROP TABLE "Attendance";
+ALTER TABLE "new_Attendance" RENAME TO "Attendance";
+CREATE UNIQUE INDEX "Attendance_userId_date_key" ON "Attendance"("userId", "date");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
